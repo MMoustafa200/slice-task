60a46022afae63ebec51d3ad67f1bf06d460b5c5
Mohamed Moustafa <mohamed.moustafa.unifi.solutions>
2024-01-30 09:32:22 +0200

    company-info API

--- src/controllers/info.controller.ts
+++ src/controllers/info.controller.ts
@@ -1,10 +1,18 @@
 import { injectable } from 'tsyringe';
 import { InfoService } from '../services/info.service';
 import { NextFunction, Request, Response } from 'express';
+import { ProcessResult } from '../common/interfaces';
 
 @injectable()
 export class InfoController {
     constructor(private readonly infoService: InfoService) {}
 
-    async getInfo(req: Request, res: Response, next: NextFunction) {}
+    async getInfo(req: Request, res: Response, next: NextFunction) {
+        const info = this.infoService.getCompanyInfo();
+
+        res.status(200).json(<ProcessResult>{
+            success: true,
+            data: { info },
+        });
+    }
 }
--- src/services/info.service.ts
+++ src/services/info.service.ts
@@ -3,4 +3,9 @@ import { injectable } from 'tsyringe';
 @injectable()
 export class InfoService {
     constructor() {}
+
+    getCompanyInfo() {
+        const info = 'Some information about the <p>company</p>.';
+        return info;
+    }
 }
