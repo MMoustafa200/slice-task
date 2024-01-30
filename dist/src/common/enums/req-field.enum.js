db231e723fdaf39c73d0b95082374cb04e2cdf20
Mohamed Moustafa <mohamed.moustafa.unifi.solutions>
2024-01-30 21:00:00 +0200

    delete dist folder

--- dist/src/common/enums/req-field.enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ReqField = void 0;
-var ReqField;
-(function (ReqField) {
-    ReqField["Params"] = "params";
-    ReqField["Query"] = "query";
-    ReqField["Body"] = "body";
-})(ReqField || (exports.ReqField = ReqField = {}));
